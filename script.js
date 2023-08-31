52c323fcef14e3ae832f9b74844c420267cff611
MRer009 <133023962+MRer009@users.noreply.github.com>
2023-08-31 17:46:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+  if (num === 0) {
+    return '0';
+  } else if (num === 1) {
+    return '1';
+  } else if (num < 0 || !Number.isInteger(num)) {
+    return 'Invalid input';
+  }
+
+  let binary = '';
+  while (num > 0) {
+    binary = (num % 2) + binary;
+    num = Math.floor(num / 2);
+  }
+
+  return binary;
 }
 
 window.decimalToBinary = decimalToBinary;
